5beff2445685b035ed2ad8455e1f3d03706287cc
hankwu0605 <hankwu0605@gmail.com>
2019-06-19 02:18:08 +0800

    add code

---
+++ kafka-demo/src/main/java/com/hank/kafka/kafkademo/controllers/KafkaController.java
@@ -0,0 +1,24 @@
+package com.hank.kafka.kafkademo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.hank.kafka.kafkademo.services.Producer;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final Producer producer;
+
+    @Autowired
+    public KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
